a9ba0d88d31a6aa061ec2bbd07d4f5ca950ceb63
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-07-05 11:36:29 +0530

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+    return(
+        <>
+        <div className="footer">
+            Copyright @{new Date().getFullYear()}
+        </div>
+        </>
+    );
+}
+
+export default Footer;
