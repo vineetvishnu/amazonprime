ff11294af46b447c7454ff1af7e5239be659bf97
Vineet Vishnu <80834138+vineetvishnu@users.noreply.github.com>
2021-07-05 09:25:53 +0530

    Added Key in Cdata.jsx

--- src/Cdata.jsx
+++ src/Cdata.jsx
@@ -6,36 +6,43 @@ import mirzapurs1 from "./images/mirzapurs1.jpg";
 import mirzapur_s2 from "./images/mirzapur_s2.jpg";
 const Cdata=[
     {
+        key: 1,
         imgsrc: breathe,
         alternate: "Breathe Poster",
         ctitle: "Breathe",
         refer: "https://www.primevideo.com/detail/0LPIP7N3YF6AF0641BL3I71ULB/ref=atv_sr_fle_c_srb7c327_brws_1_1"
     },
     {
+        key: 2,
         imgsrc: patal_lok,
         alternate: "Patal Lok Poster",
         ctitle: "Patal Lok",
         refer: "https://www.primevideo.com/detail/0G9IEOHCN8KMY6COD9ILGH7IY5/ref=atv_sr_fle_c_Tn74RA__1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B087RQLTBZ&qid=1625201010"
 
-    },{
+    },
+    {
+        key: 3,
         imgsrc: family_man_s1 ,
         alternate: "Family Man Poster",
         ctitle: "The Family Man Season 1",
         refer : "https://www.primevideo.com/search/ref=atv_sr_sug_15?phrase=family%20man%20season%201&ie=UTF8"
     },
     {
+        key: 4,
         imgsrc: family_man_s2,
         alternate: "Family Man Poster",
         ctitle: "The Family Man Season 2",
         refer :"https://www.primevideo.com/detail/0H3DDB4KBJFNDCKKLHNRLRLVKQ/ref=atv_sr_fle_c_Tn74RA__1_1_1?sr=1-1&pageTypeIdSource=ASIN&pageTypeId=B08T76677S&qid=1625201084"   
     },
     {
+        key: 5,
         imgsrc: mirzapurs1,
         alternate: "Mirzapur s1 Poster",
         ctitle:  "Mirzapur Season 1" ,
         refer: "https://www.primevideo.com/detail/0PDOKMV9CRLOMO5EUKNCUJLG4Q/ref=atv_dp_season_select_s1"  
     },
     {
+        key: 6,
         imgsrc: mirzapur_s2,
         alternate: "Mirzapur s2 Poster",
         ctitle:  "Mirzapur Season 2",
